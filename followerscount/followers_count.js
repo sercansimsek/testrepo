a14eef9f956f4d6f5278a75826af90ff0047ba11
sercansimsek <sercansimsek44@gmail.com>
2024-03-22 18:20:03 +0100

    reset functionality added to the followers count task

--- followerscount/followers_count.js
+++ followerscount/followers_count.js
@@ -17,3 +17,9 @@ function checkCountValue() {
     alert('Your Instagram post gained 10 followers! Keep it up!');
   }
 }
+
+function resetCount() {
+  document.getElementById('countDisplay').innerHTML = 0;
+
+  alert('Followers count has been reset');
+}
